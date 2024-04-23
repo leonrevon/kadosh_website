30ba62f0977c14c459ba994b7509063321d6c1c9
Ministry <tengministry@Ministry-Air.fritz.box>
2024-04-23 22:09:58 +0200

    feat: welcome message in home view

--- lib/ui/views/home/home_view.tablet.dart
+++ lib/ui/views/home/home_view.tablet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kadosh_website/ui/common/app_colors.dart';
+import 'package:kadosh_website/ui/common/ui_helpers.dart';
+import 'package:kadosh_website/ui/views/home/welcome_message.dart';
 import 'package:kadosh_website/ui/widgets/centered_view.dart';
 import 'package:kadosh_website/ui/widgets/navigation_bar/web_navigation_bar.dart';
 import 'package:stacked/stacked.dart';
@@ -11,12 +13,39 @@ class HomeViewTablet extends ViewModelWidget<HomeViewModel> {
 
   @override
   Widget build(BuildContext context, HomeViewModel viewModel) {
-    return const Scaffold(
+    return Scaffold(
         backgroundColor: kcPrimaryColor,
         body: CenteredView(
             isMobile: false,
-            child: WebNavigationBar(
-              isTablet: true,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const WebNavigationBar(
+                    isTablet: false,
+                  ),
+                  verticalSpaceMedium,
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: largeSize, right: largeSize),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: screenWidth(context),
+                          child: const WelcomeMessage(),
+                        ),
+                        verticalSpaceSmall,
+                        Image.asset(
+                          'assets/team.jpg',
+                          width: screenWidth(context),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             )));
   }
 }
