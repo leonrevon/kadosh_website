3019ebf00be3c8ca741118f26f6b94c87a7d4977
Ministry <tengministry@Ministry-Air.fritz.box>
2024-06-02 21:39:59 +0200

    feat: added about page navigation logic

--- lib/ui/widgets/navigation_bar/web_navigation_bar.dart
+++ lib/ui/widgets/navigation_bar/web_navigation_bar.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:kadosh_website/app/app.locator.dart';
+import 'package:kadosh_website/app/app.router.dart';
+import 'package:kadosh_website/extensions/hover_extensions.dart';
 import 'package:kadosh_website/ui/common/ui_helpers.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class WebNavigationBar extends StatelessWidget {
   const WebNavigationBar({super.key});
@@ -38,11 +42,27 @@ class _NavBarItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: const TextStyle(
-        fontSize: 18,
+    final routerService = locator<RouterService>();
+    return GestureDetector(
+      onTap: () => _navigateTo(title, routerService: routerService),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 18,
+        ),
       ),
-    );
+    ).moveOnHover(y: -5.0).scaleOnHover(scale: 1.2);
+  }
+
+  _navigateTo(String title, {required RouterService routerService}) {
+    switch (title) {
+      case 'Home':
+        routerService.navigateToHomeView();
+      case 'About':
+        routerService.replaceWithAboutView();
+      //TODO: other navigation tab
+      default:
+        routerService.navigateToHomeView();
+    }
   }
 }
