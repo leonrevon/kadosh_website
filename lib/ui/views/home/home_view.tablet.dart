9cf223dfbccc665741e078cc84495b9d13cad777
Leon <tengministry@Leons-MacBook-Pro.local>
2026-01-30 16:55:24 +0100

    fix: remove orange sky background

--- lib/ui/views/home/home_view.tablet.dart
+++ lib/ui/views/home/home_view.tablet.dart
@@ -26,43 +26,29 @@ class _HomePageTabletContent extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Stack(
-          alignment: AlignmentDirectional.center,
+        Column(
           children: [
-            Opacity(
-              opacity: 0.2,
-              child: Image.asset(
-                'assets/orange-sky.jpg',
-                fit: BoxFit.cover,
-                height: screenHeight(context),
-                width: screenWidth(context),
-              ),
+            Image.asset(
+              'assets/kadosh-logo.png',
             ),
-            Column(
-              children: [
-                Image.asset(
-                  'assets/kadosh-logo.png',
-                ),
-                verticalSpaceMedium,
-                Text(
-                  l10n.homeTagline,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    height: 0.9,
-                    fontSize: getResponsiveLargeFontSize(context),
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                verticalSpaceMedium,
-                Text(
-                  l10n.homeSubtitle,
-                  style: TextStyle(
-                    fontSize: getResponsiveLargeFontSize(context),
-                    height: 1.7,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+            verticalSpaceMedium,
+            Text(
+              l10n.homeTagline,
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                height: 0.9,
+                fontSize: getResponsiveLargeFontSize(context),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            verticalSpaceMedium,
+            Text(
+              l10n.homeSubtitle,
+              style: TextStyle(
+                fontSize: getResponsiveLargeFontSize(context),
+                height: 1.7,
+              ),
+              textAlign: TextAlign.center,
             ),
           ],
         ),
